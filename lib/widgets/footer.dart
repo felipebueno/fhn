90241ed3a18d42ba604f04abe343f1acba862cfd
Felipe Bueno <bueno.felipe@gmail.com>
2020-03-30 22:43:05 -0300

    Cleanup the Footer

--- lib/widgets/footer.dart
+++ lib/widgets/footer.dart
@@ -1,7 +1,9 @@
 import 'package:fhn/constants.dart';
+import 'package:fhn/utils.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
 import 'package:flutter/rendering.dart';
+import 'package:social_media_buttons/social_media_icons.dart';
 
 class Footer extends StatelessWidget {
   const Footer({
@@ -10,43 +12,55 @@ class Footer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return Material(
       color: kHNOrange,
-      width: double.infinity,
       child: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: <Widget>[
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: <Widget>[
-                Text(
-                  'About',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                Text(
-                  'Contact',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ],
-            ),
-            SizedBox(height: 16.0),
-            Text(
-              'by Felipe Bueno',
-              style: TextStyle(
-                color: Colors.white,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ],
+        child: InkWell(
+          onTap: () => Utils.launchURL('https://github.com/felipebueno/fhn'),
+          child: IconText(
+            icon: SocialMediaIcons.github_circled,
+            text: 'Source',
+            color: Colors.white,
+            horizontalAlignment: MainAxisAlignment.center,
+          ),
         ),
       ),
+    );
+  }
+}
+
+class IconText extends StatelessWidget {
+  final IconData icon;
+  final String text;
+  final Color color;
+  final MainAxisAlignment horizontalAlignment;
+
+  const IconText({Key key,
+    @required this.icon,
+    this.color = Colors.black,
+    @required this.text,
+    this.horizontalAlignment = MainAxisAlignment.start})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: this.horizontalAlignment,
+      children: <Widget>[
+        Icon(
+          this.icon,
+          color: this.color,
+        ),
+        SizedBox(width: 4),
+        Text(
+          this.text,
+          style: TextStyle(
+            color: this.color,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ],
     );
   }
 }
